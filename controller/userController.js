530a8461185b81078d91afcfb502a526ac6c440f
Your Name <you@example.com>
2024-05-26 07:24:17 -0700

    secound-commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,47 +1,59 @@
 import userModel from "../models/userModel.js";
 
+// User Signup Controller
 export const userSignup = async (req, res) => {
-    // console.log(req.body);
-    const { email } = req.body;
+    try {
+        const { email } = req.body;
 
-    userModel.findOne({ email: email }, (err, result) => {
-        // console.log(result);
-        console.log(err);
-        if (result) {
-            res.send({ message: "Email id is already register", alert: false });
-        } else {
-            const data = userModel(req.body);
-            const save = data.save();
-            res.send({ message: "Successfully sign up", alert: true });
+        // Check if the user already exists
+        const existingUser = await userModel.findOne({ email: email });
+
+        if (existingUser) {
+            return res.status(400).send({ message: "Email id is already registered", alert: false });
         }
-    });
+
+        // Create a new user
+        const newUser = new userModel(req.body);
+        await newUser.save();
+
+        res.status(201).send({ message: "Successfully signed up", alert: true });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal server error", alert: false });
+    }
 };
 
+// User Login Controller
+export const userLogin = async (req, res) => {
+    try {
+        const { email } = req.body;
 
-//login  controller  
-export const userLogin = (req, res) => {
-    // console.log(req.body);
-    const { email } = req.body;
-    userModel.findOne({ email: email }, (err, result) => {
-        if (result) {
+        // Find the user by email
+        const user = await userModel.findOne({ email: email });
+
+        if (user) {
             const dataSend = {
-                _id: result._id,
-                firstName: result.firstName,
-                lastName: result.lastName,
-                email: result.email,
-                image: result.image,
+                _id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                image: user.image,
             };
+
             console.log(dataSend);
-            res.send({
-                message: "Login is successfully",
+            res.status(200).send({
+                message: "Login is successful",
                 alert: true,
                 data: dataSend,
             });
         } else {
-            res.send({
+            res.status(400).send({
                 message: "Email is not available, please sign up",
                 alert: false,
             });
         }
-    });
-}
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal server error", alert: false });
+    }
+};
